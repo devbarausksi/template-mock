3cff8ed012ecfdcfecaa4cb046a5ac00d0e783d3
Bruno de Araújo Alves <b.araujo@kingspanisoeste.com.br>
2023-11-21 17:56:56 -0300

    feat: ✨ render apollo studio on /graphiql

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
+import cors from 'cors'
 import express, { type Express } from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import morgan from 'morgan'
@@ -19,16 +20,31 @@ app.use(
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   graphqlHTTP({
     schema,
-    graphiql: true,
-    pretty: true,
   }),
 )
 
+app.get('/graphiql', (req, res) => {
+  res.send(`
+  <div id="sandbox" style="position:absolute;top:0;right:0;bottom:0;left:0"></div>
+  <script src="https://embeddable-sandbox.cdn.apollographql.com/_latest/embeddable-sandbox.umd.production.min.js"></script>
+  <script>
+   new window.EmbeddedSandbox({
+     target: "#sandbox",
+     // Pass through your server href if you are embedding on an endpoint.
+     // Otherwise, you can pass whatever endpoint you want Sandbox to start up with here.
+     initialEndpoint: '/graphql',
+   });
+   // advanced options: https://www.apollographql.com/docs/studio/explorer/sandbox#embedding-sandbox
+  </script>
+  `)
+})
+
 app.use(morgan('dev'))
+app.use(cors())
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(
-    `⚡️[server]: Server is running at http://localhost:${port}/graphql`,
+    `⚡️[server]: Server is running at http://localhost:${port}/graphiql`,
   )
 })
